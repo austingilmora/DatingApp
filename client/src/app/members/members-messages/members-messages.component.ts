cc76eb1c3ad2de3e7c34d6215bb71cb4a1440a23
Austin Gilmore <agilmore@relias.com>
2023-06-23 16:07:27 -0400

    End of Section 15

---
+++ client/src/app/members/members-messages/members-messages.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Message } from 'src/app/_models/message';
+import { MessageService } from 'src/app/_services/message.service';
+
+@Component({
+  selector: 'app-members-messages',
+  templateUrl: './members-messages.component.html',
+  styleUrls: ['./members-messages.component.css']
+})
+export class MembersMessagesComponent {
+  @ViewChild('messageForm') messageForm?: NgForm;
+  @Input() username?: string;
+  @Input() messages: Message[] = [];
+  messageContent = '';
+
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit(): void {
+  }
+
+  sendMessage() {
+    if (!this.username) return;
+    this.messageService.sendMessage(this.username, this.messageContent).subscribe({
+      next: message => {
+        this.messages.push(message)
+        this.messageForm?.reset();
+      }
+    })
+  }
+}
