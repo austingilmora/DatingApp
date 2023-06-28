a54afa8518dae789c6d83b97ac66fe92e063bf7a
Austin Gilmore <agilmore@relias.com>
2023-06-28 15:35:54 -0400

    End of Section 16

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -26,6 +26,11 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
 import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { DatePickerComponent } from './_forms/date-picker/date-picker.component';
 import { MembersMessagesComponent } from './members/members-messages/members-messages.component';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { HasRoleDirective } from './_directives/has-role.directive';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
+import { RolesModalComponent } from './modals/roles-modal/roles-modal.component';
 
 @NgModule({
   declarations: [
@@ -45,7 +50,12 @@ import { MembersMessagesComponent } from './members/members-messages/members-mes
     PhotoEditorComponent,
     TextInputComponent,
     DatePickerComponent,
-    MembersMessagesComponent
+    MembersMessagesComponent,
+    AdminPanelComponent,
+    HasRoleDirective,
+    UserManagementComponent,
+    PhotoManagementComponent,
+    RolesModalComponent
   ],
   imports: [
     BrowserAnimationsModule,
