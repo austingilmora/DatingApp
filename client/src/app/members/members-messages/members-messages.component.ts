dc9818bd6f929e721a611c2316b95e0952fba307
Austin Gilmore <agilmore@relias.com>
2023-07-12 17:10:16 -0400

    End of Section 18

--- client/src/app/members/members-messages/members-messages.component.ts
+++ client/src/app/members/members-messages/members-messages.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Message } from 'src/app/_models/message';
 import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-members-messages',
   templateUrl: './members-messages.component.html',
   styleUrls: ['./members-messages.component.css']
